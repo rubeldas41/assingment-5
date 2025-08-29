62dd3d993e37bfdd96d49a7b3385344aa6ef10e7
Rubel Chandra Das <rubeldasx@gmail.com>
2025-08-29 07:13:36 +0600

    added all files

--- javacript.js
+++ javacript.js
@@ -14,3 +14,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const clearHistoryBtn = document.getElementById('clearHistoryBtn');
 
   coinCountEl.textContent = coinCount;
+   // ❤️ Like button
+  likeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      heartCount++;
+      heartCountEl.textContent = heartCount;
+    });
+  });
+
+  // Call button (coin loss)
+  callButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const number = button.getAttribute('data-number');
+      const service = button.getAttribute('data-service');
+
+      if (coinCount >= 20) {
+        coinCount -= 20;
+        coinCountEl.textContent = coinCount;
+        alert("Calling" +" " + service +" "+ number+ "..")
+
+        const callTime = new Date().toLocaleTimeString();
+        callHistory.push({ service, number, time: callTime });
+        renderCallHistory();
+      } else {
+        alert("Not enough coins to make a call!");
+      }
+    });
+  });
